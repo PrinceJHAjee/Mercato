e2957346ed6df41e0559e6482462a7e1b92f9785
PrinceJHAjee <princejee0710@gmail.com>
2024-08-26 23:03:24 +0530

    Fixed warnings and optimized build

--- client/src/apicalls/AxiosInstance.js
+++ client/src/apicalls/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const axiosInstance =axios.create({ 
     
     //making this global axios Instance object  so tha it call be use for all end points
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
     headers: {
           authorization: `Bearer ${localStorage.getItem('token')}` //std way to send auth token to backend
     }
